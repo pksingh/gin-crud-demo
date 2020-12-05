259738408252f8d90eaa373d8a79534c56f08d8b
Prasanta Singh <singh.prasanta@gmail.com>
2020-12-06 00:11:23 +0530

    Read from ENV param else from config for DB

--- startup/db/pgdb.go
+++ startup/db/pgdb.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/jackc/pgconn"
@@ -28,19 +29,38 @@ func InitDB(ctx context.Context) error {
 	logger := log.GetLogger(ctx)
 	props := appProps.GetAll()
 
-	host := props.GetString("postgresql.host", "")
-	dbname := props.GetString("postgresql.dbname", "")
-	port := strconv.Itoa(props.GetInt("postgresql.port", 0))
-	user := props.GetString("postgresql.user", "")
-	pwd := props.GetString("postgresql.pwd", "")
-	sslmode := props.GetString("postgresql.sslmode", "")
+	host, ok := os.LookupEnv("DATABASE_HOST")
+	if !ok {
+		host = props.GetString("postgresql.host", "")
+	}
+	dbname, ok := os.LookupEnv("DATABASE_NAME")
+	if !ok {
+		dbname = props.GetString("postgresql.dbname", "")
+	}
+	port, ok := os.LookupEnv("DATABASE_PORT")
+	if !ok {
+		port = strconv.Itoa(props.GetInt("postgresql.port", 0))
+	}
+	user, ok := os.LookupEnv("DATABASE_USER")
+	if !ok {
+		user = props.GetString("postgresql.user", "")
+	}
+	cleartextPwd, ok := os.LookupEnv("DATABASE_PASSWORD")
+	if !ok {
+		cleartextPwd = props.GetString("postgresql.pwd", "")
+	}
+
+	sslmode, ok := os.LookupEnv("DATABASE_SSLMODE")
+	if !ok {
+		sslmode = props.GetString("postgresql.sslmode", "")
+	}
 
 	logger.Info("connection string:", zap.String("host", host), zap.String("port", port),
 		zap.String("user", user), zap.String("dbname", dbname), zap.String("sslmode", sslmode))
 
-	fmt.Println("cleartextPwd PWD=", pwd)
+	fmt.Println("cleartextPwd PWD=", cleartextPwd)
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		host, port, user, pwd, dbname, sslmode)
+		host, port, user, cleartextPwd, dbname, sslmode)
 
 	var err error
 	Postgres, err = pgx.Connect(ctx, psqlInfo)

cde687078ecc5ef254921dc398259a541d6ac894
Prasanta Singh <singh.prasanta@gmail.com>
2020-12-20 12:12:28 +0530

    GetAllUsers() defined in model.

--- model/user_table.go
+++ model/user_table.go
@@ -1,7 +1,13 @@
 package model
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pksingh/gin-curd-demo/startup/db"
 )
 
 type UserInfo struct {
@@ -23,4 +29,56 @@ type UserInfo struct {
 	Update_rcd_by_who string    `json:"u_update_rcd_by_who"`
 	Update_rcd_by_app string    `json:"u_update_rcd_by_app"`
 	Data_source       string    `json:"u_data_source"`
+}
+
+func GetAllUsers(c *gin.Context) ([]UserInfo, error) {
+	var uinfos []UserInfo
+	conn := db.Postgres
+
+	query := "SELECT * FROM user_info"
+	fmt.Println("Query: ", query)
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	count := 0
+	// iterate through the rows
+	for rows.Next() {
+		count++
+		values, err := rows.Values()
+		if err != nil {
+			return nil, err
+		}
+
+		// convert DB types to Go types
+		uinfo := UserInfo{
+			User_id:           values[0].(int64),
+			Account_id:        values[1].(int64),
+			Contact_id:        values[2].(int64),
+			Loyalty_id:        values[3].(int64),
+			IsActive_id:       values[4].(bool),
+			Reference_id:      values[5].(int64),
+			User_type:         values[6].(string),
+			Account_type:      values[7].(string),
+			Loyalty_type:      values[8].(string),
+			Member_type:       values[9].(string),
+			Brand_type:        values[10].(string),
+			Create_rcd_at:     values[11].(time.Time),
+			Create_rcd_by_who: values[12].(string),
+			Create_rcd_by_app: values[13].(string),
+			Update_rcd_at:     values[14].(time.Time),
+			Update_rcd_by_who: values[15].(string),
+			Update_rcd_by_app: values[16].(string),
+			Data_source:       values[17].(string),
+		}
+
+		uinfos = append(uinfos, uinfo)
+	}
+
+	if count > 0 {
+		return uinfos, nil
+	} else {
+		return nil, errors.New("record NOT Found")
+	}
 }

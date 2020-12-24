25dc30813b9e1379c7a25b795f9393dd81dc060a
Prasanta Singh <singh.prasanta@gmail.com>
2020-12-24 12:27:51 +0530

    GetSingleUser() defined in model

--- model/user_table.go
+++ model/user_table.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -81,4 +82,59 @@ func GetAllUsers(c *gin.Context) ([]UserInfo, error) {
 	} else {
 		return nil, errors.New("record NOT Found")
 	}
+}
+
+func GetSingleUser(c *gin.Context) (*UserInfo, error) {
+	var uinfo *UserInfo
+	conn := db.Postgres
+
+	user_id := c.Query("user_id")
+	// account_id := c.Query("account_id")
+	// contact_id := c.Query("contact_id")
+	// loyalty_id := c.Query("loyalty_id")
+
+	// fmt.Printf("user_id: %s; account_id: %s; contact_id: %s; loyalty_id: %s \n", user_id, account_id, contact_id, loyalty_id)
+	fmt.Printf("user_id: %s\n", user_id)
+	if user_id == "" {
+		return nil, errors.New("all mandatory values NOT Passed")
+	}
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM user_info WHERE u_user_id=$1 LIMIT 1", user_id)
+	if err != nil {
+		log.Fatal("error while executing query")
+		return nil, err
+	}
+
+	// iterate through the rows
+	for rows.Next() {
+		values, err := rows.Values()
+		if err != nil {
+			log.Fatal("error while iterating dataset")
+		}
+
+		// convert DB types to Go types
+		uinfo = &UserInfo{
+			User_id:           values[0].(int64),
+			Account_id:        values[1].(int64),
+			Contact_id:        values[2].(int64),
+			Loyalty_id:        values[3].(int64),
+			IsActive_id:       values[4].(bool),
+			Reference_id:      values[5].(int64),
+			User_type:         values[6].(string),
+			Account_type:      values[7].(string),
+			Loyalty_type:      values[8].(string),
+			Member_type:       values[9].(string),
+			Brand_type:        values[10].(string),
+			Create_rcd_at:     values[11].(time.Time),
+			Create_rcd_by_who: values[12].(string),
+			Create_rcd_by_app: values[13].(string),
+			Update_rcd_at:     values[14].(time.Time),
+			Update_rcd_by_who: values[15].(string),
+			Update_rcd_by_app: values[16].(string),
+			Data_source:       values[17].(string),
+		}
+		log.Println("[User_id:", uinfo.User_id, "]")
+	}
+
+	return uinfo, nil
 }

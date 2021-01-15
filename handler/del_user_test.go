e1b7d5aba41a9dfd10f5e2972635a488f632b102
Prasanta Singh <singh.prasanta@gmail.com>
2021-01-15 22:34:34 +0530

    del_user_test.go : TestDeleteUser() TC added

---
+++ handler/del_user_test.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"context"
+	glog "log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pksingh/gin-curd-demo/log"
+	"github.com/pksingh/gin-curd-demo/startup/appProps"
+	"github.com/pksingh/gin-curd-demo/startup/db"
+)
+
+func TestDeleteUser(t *testing.T) {
+	ctx := context.TODO()
+	_ = os.Setenv("runEnv", "dev")
+	_ = appProps.Load("./../resources")
+	_ = log.Load(ctx)
+	_ = db.Load(ctx)
+	router := gin.New()
+	gin.SetMode(gin.TestMode)
+	router.GET("/user", DeleteUser)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user?user_id=3", nil)
+	router.ServeHTTP(w, req)
+	glog.Println("resp: ", w)
+	assert.NotEmpty(t, w)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, w.Body)
+
+	str := w.Body.String()
+	assert.NotContains(t, str, "error")
+	assert.NotContains(t, str, "record NOT Found")
+
+	// assert.JSONEq(t,appInfo.String(), w.Body.String())
+}

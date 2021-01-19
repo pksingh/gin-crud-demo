6c897af8d6a2799cb37fb0bce045635f2ff62da9
Prasanta Singh <singh.prasanta@gmail.com>
2021-01-19 22:38:54 +0530

    TestDeleteUserBadRequest() TC defined

--- handler/del_user_test.go
+++ handler/del_user_test.go
@@ -88,3 +88,28 @@ func TestDeleteUserNoRecordFound(t *testing.T) {
 	assert.Contains(t, str, "record NOT Found")
 	// assert.JSONEq(t,appInfo.String(), w.Body.String())
 }
+
+func TestDeleteUserBadRequest(t *testing.T) {
+	ctx := context.TODO()
+	_ = os.Setenv("runEnv", "dev")
+	_ = os.Setenv("DATABASE_NAME", "nil")
+	_ = appProps.Load("./../resources")
+	_ = log.Load(ctx)
+	_ = db.Load(ctx)
+	router := gin.New()
+	gin.SetMode(gin.TestMode)
+	router.GET("/user", DeleteUser)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user?user_id=12345", nil)
+	router.ServeHTTP(w, req)
+	glog.Println("resp: ", w)
+	assert.NotEmpty(t, w)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.NotEmpty(t, w.Body)
+
+	str := w.Body.String()
+	assert.Contains(t, str, "error")
+	assert.Contains(t, str, "ERROR")
+	assert.Contains(t, str, "SQLSTATE")
+	// assert.JSONEq(t,appInfo.String(), w.Body.String())
+}

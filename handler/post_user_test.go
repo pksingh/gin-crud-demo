ee49f6855b1e677430ba093ad1e76b0cb995a6f7
Prasanta Singh <singh.prasanta@gmail.com>
2021-02-08 11:15:29 +0530

    TestInsertUserInvalidRequest() changes

--- handler/post_user_test.go
+++ handler/post_user_test.go
@@ -109,8 +109,7 @@ func TestInsertUserInvalidRequest(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	router.POST("/user", InsertUser)
 	w := httptest.NewRecorder()
-	// body := "{\"u_user_id\":1,\"u_account_id\":21,\"u_contact_id\":22,\"u_loyalty_id\":23,\"u_is_active_id\":true,\"u_reference_id\":21,\"u_user_type\":\"u\",\"u_account_type\":\"a\",\"u_loyalty_type\":\"l\",\"u_member_type\":\"m\",\"u_brand_type\":\"b\",\"u_create_rcd_at\":\"2022-09-13T15:11:48+05:30\",\"u_create_rcd_by_who\":\"w\",\"u_create_rcd_by_app\":\"a\",\"u_update_rcd_at\":\"2022-09-13T15:12:12+05:30\",\"u_update_rcd_by_who\":\"w\",\"u_update_rcd_by_app\":\"a\",\"u_data_source\":\"s\"}"
-	body := "{\"u_user_id\":1,\"u_account_id\":21,\"u_contact_id\":22,\"u_loyalty_id\":23,\"u_is_active_id\":true,\"u_reference_id\":21}"
+	body := "{\"u_user_id\":1}"
 	req, _ := http.NewRequest(http.MethodPost, "/user?user_id=3", strings.NewReader(body))
 	router.ServeHTTP(w, req)
 	glog.Println("resp: ", w)
@@ -120,12 +119,9 @@ func TestInsertUserInvalidRequest(t *testing.T) {
 
 	str := w.Body.String()
 	assert.NotContains(t, str, "record NOT Found")
-
 	assert.NotContains(t, str, "message")
 	assert.NotContains(t, str, "SUCCESS")
 
 	assert.Contains(t, str, "error")
-	assert.Contains(t, str, "executing")
-	assert.Contains(t, str, "INSERT")
-	// assert.JSONEq(t,appInfo.String(), w.Body.String())
+	assert.Contains(t, str, "validation")
 }
